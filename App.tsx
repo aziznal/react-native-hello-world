d1eee1e1a5e9580cde38490bbbf820d2d57ad38e
Aziz <aziznal.dev@gmail.com>
2023-10-08 14:23:58 +0300

    setup basic view with styles and state

--- App.tsx
+++ App.tsx
@@ -1,19 +1,39 @@
 import React from 'react';
 import {
+  Button,
   SafeAreaView,
   ScrollView,
   StatusBar,
+  StyleProp,
   Text,
+  TextStyle,
   useColorScheme,
+  ViewStyle,
 } from 'react-native';
-
-import {Colors} from 'react-native/Libraries/NewAppScreen';
 
 function App(): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
 
+  const [count, setCount] = React.useState(0);
+
   const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    backgroundColor: isDarkMode ? '#2f2f2f' : 'white',
+  };
+
+  const textStyle: StyleProp<TextStyle> = {
+    fontSize: 30,
+    color: isDarkMode ? 'white' : 'black',
+    fontWeight: 'bold',
+    textAlign: 'center',
+    padding: 20,
+  };
+
+  const centeredColumnStyle: StyleProp<ViewStyle> = {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100%',
   };
 
   return (
@@ -22,10 +42,11 @@ function App(): JSX.Element {
         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
         backgroundColor={backgroundStyle.backgroundColor}
       />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Text>Hello World!</Text>
+
+      <ScrollView contentContainerStyle={centeredColumnStyle}>
+        <Text style={textStyle}>Welcome to React Native</Text>
+        <Text style={textStyle}>Here's a counter: {count}</Text>
+        <Button title="Increment" onPress={() => setCount(c => c + 1)} />
       </ScrollView>
     </SafeAreaView>
   );
